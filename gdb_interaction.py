fc562e6568ca4b8b6d7e62a57debde35e7872eab
root <root@localhost.localdomain>
2019-04-28 18:35:26 -0400

    Add methods

--- gdb_interaction.py
+++ gdb_interaction.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from pwn import *
 
+functs_vulns =['gets','strcpy','scanf','strncpy']
 asm_funct_call = {}
-time_out = 0.2
+time_out = 0.1
 
 #ejecuta gdb
 def exec_gdb(file_name):
@@ -14,20 +15,43 @@ def print_stdout(lines):
         print(line.decode('utf-8'))
 
 #identifica si la linea asm tiene un call
-def idenfi_call_lines(line):
+def identify_call_lines(line):
     if 'call' in line:
         return True
     return False
 
-#
-def
+#Renoce la funcion
+def rec_funct(field):
+    for fun in functs_vulns:
+        if fun in field:
+            return fun
+    return False
 
-#mapea las funciones con los opcodes call
+#separa los campos de las lineas con call, devolviendo los necesarios
+def parse_call_line(line):
+    fields = line.split()
+    break_direction = fields[1][fields[1].index('<')+1:fields[1].index('>')]
+    fun_name = rec_funct(fields[-1])
+    return (fun_name, break_direction)
+
+
+#mapea las funciones con los opcodes call y las agrega a la lista
 def map_functions(gdb , funtions_dict):
     for fun in funtions_dict.keys():
+        asm_funct_call[fun] = []
         gdb.sendline('disass {}'.format(fun))
         lines = gdb.recvlines(timeout=time_out)
+
+        #print('-------------------------------------------------')
+        #print_stdout(lines)
+        #print('-------------------------------------------------')
+
         for line in lines:
+            line = line.decode('utf-8')
+            if identify_call_lines(line):
+                x = parse_call_line(line)
+                if x[0]:
+                    asm_funct_call[fun].append(x)
 
 
 #funcion general que realiza la fase de reconocimiento
@@ -36,12 +60,21 @@ def run_recognition_phase(executable, funtions_dict):
         gdb.recvlines(timeout=time_out)
         #print_stdout(gdb.recvlines(timeout=0.2))
 
+
+        gdb.sendline('set height unlimited')
+        gdb.recvlines(timeout=time_out)
+
+        gdb.sendline('set width unlimited')
+        gdb.recvlines(timeout=time_out)
+
         gdb.sendline('b main')
         gdb.recvlines(timeout=time_out)
+        #print('>b main')
         #print_stdout(gdb.recvlines(timeout=0.2))
 
         gdb.sendline('run')
         gdb.recvlines(timeout=time_out)
+        #print('>run')
         #print_stdout(gdb.recvlines(timeout=0.2))
 
         map_functions(gdb, funtions_dict)
@@ -49,6 +82,7 @@ def run_recognition_phase(executable, funtions_dict):
 
         gdb.close()
 
-        run_recognition_phase('../x.out', d)
 if __name__ == '__main__':
     d = {'r':[], 'main':[]}
+    run_recognition_phase('../x.out', d)
+    print(asm_funct_call)
